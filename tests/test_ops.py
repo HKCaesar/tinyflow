b9d672fb3fb94602b9eaf298197c812ea63df0e4
Kevin Wurster <wursterk@gmail.com>
2017-02-24 23:19:49 -0500

    Consolidate simple operation tests.

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -19,18 +19,6 @@ def test_description():
     assert 'description' >> ops.Operation()
 
 
-def test_Filter():
-    values = [0, 1, 2, None, 4]
-    tform = ops.filter(bool)
-    assert (1, 2, 4) == tuple(tform(values))
-
-
-def test_Map():
-    values = tuple(range(5))
-    tform = ops.map(lambda x: x ** 2)
-    assert tuple(map(lambda x: x ** 2, values)) == tuple(tform(values))
-
-
 def test_ReduceByKey():
 
     data = [
@@ -79,23 +67,17 @@ def test_Operation_abc():
         o([])
 
 
-def test_flatmap():
-    o = ops.flatmap(lambda x: x.upper())
-    words = ['first', 'second', 'Third']
-    actual = tuple(o(words))
-    expected = tuple(it.chain.from_iterable([w.upper() for w in words]))
-    assert expected == actual
+@pytest.mark.parametrize("operation,input_data,expected", [
+    (ops.drop(2), range(5), (2, 3, 4)),
+    (ops.take(2), range(5), (0, 1)),
+    (ops.flatmap(lambda x: x.upper()), ['w1', 'w2'], ['W', '1', 'W', '2']),
+    (ops.map(lambda x: x ** 2), (2, 4, 8), (4, 16, 64)),
+    (ops.filter(bool), (0, 1, 2, None, 4), (1, 2, 4))
+])
+def test_basic_operation(operation, input_data, expected):
 
+    """A lot of operations take few arguments are generate an only slightly
+    altered output.  Output and expected values are compared as tuples.
+    """
 
-def test_take():
-    o = ops.take(2)
-    actual = tuple(o(range(10)))
-    expected = (0, 1)
-    assert expected == actual
-
-
-def test_drop():
-    o = ops.drop(2)
-    actual = tuple(o(range(5)))
-    expected = (2, 3, 4)
-    assert expected == actual
+    assert tuple(operation(input_data)) == tuple(expected)
