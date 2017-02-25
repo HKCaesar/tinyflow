0e380383ea81ca8254045bb41b409a8c41aa185a
Kevin Wurster <wursterk@gmail.com>
2017-02-24 23:55:02 -0500

    + tinyflow.tools

---
+++ tinyflow/tools.py
@@ -0,0 +1,36 @@
+"""Assorted tools for working with streaming data."""
+
+
+import itertools as it
+
+
+def slicer(iterable, chunksize):
+
+    """
+    Read an iterator in chunks.
+    Example:
+        >>> for p in slicer(range(5), 2):
+        ...     print(p)
+        (0, 1)
+        (2, 3)
+        (4,)
+    Parameters
+    ----------
+    iterable : iter
+        Input stream.
+    chunksize : int
+        Number of records to include in each chunk.  The last chunk will be
+        incomplete unless the number of items in the stream is evenly
+        divisible by `size`.
+    Yields
+    ------
+    tuple
+    """
+
+    iterable = iter(iterable)
+    while True:
+        v = tuple(it.islice(iterable, chunksize))
+        if v:
+            yield v
+        else:
+            raise StopIteration
