c1ccc806b63f12cfaafc6aea48dcc5c7f612bea7
Kevin Wurster <wursterk@gmail.com>
2017-02-25 19:52:56 -0500

    Test for tinyflow.serial.ops.counter() that grabs all items not just the most frequent.

--- tests/test_serial_ops.py
+++ tests/test_serial_ops.py
@@ -27,6 +27,7 @@ def test_Operation_abc():
 @pytest.mark.parametrize("operation,input_data,expected", [
     (ops.itemgetter(0), (('something', None), ('none', 'else')), ('something', 'none')),
     (ops.counter(3), (1, 1, 2, 2, 4, 4, 5), ((1, 2), (2, 2), (4, 2))),
+    (ops.counter(), (1, 1, 2, 2, 4, 4, 5), ((1, 2), (2, 2), (4, 2), (5, 1))),
     (ops.drop(2), range(5), (2, 3, 4)),
     (ops.take(2), range(5), (0, 1)),
     (ops.map(lambda x: x ** 2), (2, 4, 8), (4, 16, 64)),
