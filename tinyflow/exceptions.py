e7aa9d9c853e9c6373587150c1d7015d33840de0
Kevin Wurster <wursterk@gmail.com>
2017-02-25 22:37:13 -0500

    + Some coroutine exceptions.

--- tinyflow/exceptions.py
+++ tinyflow/exceptions.py
@@ -11,3 +11,20 @@ class NotAnOperation(TinyFlowException):
     """Raise when an object should be an instance of
     ``tinyflow.ops.Operation()`` but isn't.
     """
+
+
+class NotACoroOperation(NotAnOperation):
+
+    """Like ``NotAnOperation()`` but for ``tinyflow.coro.ops``."""
+
+
+class NotACoroTarget(NotACoroOperation):
+
+    """Like ``NotACoroOperation()`` but for coroutine targets."""
+
+
+class TooManyTargets(TinyFlowException):
+
+    """Raised when a ``tinyflow.coro.CoroPipeline()`` receives too many
+    ``tinyflow.coro.ops.CoroTarget()``'s.
+    """
