48861dbacb252d4ed6baceca77e0f592cf20e2bb
Kevin Wurster <wursterk@gmail.com>
2017-03-15 01:34:55 -0400

    Give operations access to their parent pipeline.  Added parallelization to 'map()'.

--- tinyflow/exceptions.py
+++ tinyflow/exceptions.py
@@ -28,3 +28,10 @@ class TooManyTargets(TinyFlowException):
     """Raised when a ``tinyflow.coro.CoroPipeline()`` receives too many
     ``tinyflow.coro.ops.CoroTarget()``'s.
     """
+
+
+class NoPipeline(TinyFlowException):
+
+    """Raised when an operation has not been attached to a pipeline, but
+    requests its parent pipeline.
+    """
