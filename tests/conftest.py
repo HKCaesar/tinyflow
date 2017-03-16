6d2438b2c53e07f5438bf2b6cfce58bb6e956a8d
Kevin Wurster <wursterk@gmail.com>
2017-03-15 20:57:30 -0400

    pytest fixtures.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@
 import pytest
 
 from tinyflow import __license__
+from tinyflow import _testing
 
 
 @pytest.fixture(scope='module')
@@ -14,3 +15,25 @@ def wordcount_input():
 @pytest.fixture(scope='module')
 def wordcount_top5():
     return {'the': 13, 'of': 12, 'or': 11, 'and': 8, 'in': 6}
+
+
+@pytest.fixture(scope='module')
+def square():
+    """Returns a function behaving like ``lambda x: x ** 2`` to get around
+    ``pickle``'s limitations.
+    """
+    return _testing.square
+
+
+@pytest.fixture(scope='module')
+def add2():
+    """Returns a function behaving like ``lambda a, b: a + b`` to get around
+    ``pickle's`` limitations.
+    """
+    return _testing.add2
+
+
+@pytest.fixture(scope='module')
+def add4():
+    """Same as ``add2()`` but with 4 arguments."""
+    return _testing.add4
